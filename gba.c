d5a1c9a55501ec625d96c426dc2a6e98ab87d31b
clenhardt <clenhardt3@gatech.edu>
2024-07-14 15:17:04 -0400

    fixed wait for vblank by using scanline counter instead

--- gba.c
+++ gba.c
@@ -13,11 +13,11 @@ void waitForVBlank(void) {
   // (1)
   // Write a while loop that loops until we're NOT in vBlank anymore:
   // (This prevents counting one VBlank more than once if your app is too fast)
-  while(vBlankCounter >= 160 && vBlankCounter > 0);
+  while(SCANLINECOUNTER >= 160 && SCANLINECOUNTER > 0);
 
   // (2)
   // Write a while loop that keeps going until we're in vBlank:
-  while(vBlankCounter < 160);
+  while(SCANLINECOUNTER < 160);
 
   // (3)
   // Finally, increment the vBlank counter:
