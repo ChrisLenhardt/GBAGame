acd3b24019ac20672901449219d293fee25199e4
clenhardt <clenhardt3@gatech.edu>
2024-07-14 15:18:08 -0400

    implemented per lab

--- gba.c
+++ gba.c
@@ -13,11 +13,11 @@ void waitForVBlank(void) {
   // (1)
   // Write a while loop that loops until we're NOT in vBlank anymore:
   // (This prevents counting one VBlank more than once if your app is too fast)
-  while(SCANLINECOUNTER >= 160 && SCANLINECOUNTER > 0);
+  while(SCANLINECOUNTER > 160);
 
   // (2)
   // Write a while loop that keeps going until we're in vBlank:
-  while(SCANLINECOUNTER < 160);
+  while(SCANLINECOUNTER != 160);
 
   // (3)
   // Finally, increment the vBlank counter:
